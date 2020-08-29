ba6102123c9a07f8f08d49d75eae1f0a0d4526cd
novafacing <rowanbhart@gmail.com>
2020-08-29 10:21:34 -0400

    Split burp installer for github

--- src/install/web.sh
+++ src/install/web.sh
@@ -2,4 +2,5 @@ pip3 --no-cache-dir install flask setuptools
 wget -O postman.tar.gz https://dl.pstmn.io/download/latest/linux64
 tar -xzvf postman.tar.gz
 ln -s $(pwd)/Postman/Postman /bin/postman
+cat /headless/install/burp?? > /headless/install/burpsuite.sh
 ln -s /headless/install/burpsuite.sh /bin/burpsuite-install
